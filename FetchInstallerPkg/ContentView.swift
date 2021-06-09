3186bc7eb15433e2f670e7c21a35232298437769
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2021-06-09 13:47:07 +0200

    Initial Commit

---
+++ FetchInstallerPkg/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FetchInstallerPkg
+//
+//  Created by Armin Briegel on 2021-06-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FetchInstallerPkg/FetchInstallerPkgApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchInstallerPkgApp.swift
+//  FetchInstallerPkg
+//
+//  Created by Armin Briegel on 2021-06-09.
+//
+
+import SwiftUI
+
+@main
+struct FetchInstallerPkgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
